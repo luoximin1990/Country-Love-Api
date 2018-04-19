b816f4aaf2c23c48f2789b698b5d28a0e00c05c8
yangliu <yangliu@192.168.71.40>
2018-04-19 18:28:10 +0800

    update SmsUtil

--- src/main/java/com/marykay/country/love/util/SmsUtil.java
+++ src/main/java/com/marykay/country/love/util/SmsUtil.java
@@ -6,13 +6,8 @@ import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
-import com.marykay.country.love.api.contract.request.QuerySendDetailsRequest;
 import com.marykay.country.love.api.contract.request.SendSmsRequest;
-import com.marykay.country.love.api.contract.response.QuerySendDetailsResponse;
 import com.marykay.country.love.api.contract.response.SendSmsResponse;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * Created on 17/6/7. 短信API产品的DEMO程序,工程中包含了一个SmsDemo类，直接通过
@@ -71,73 +66,6 @@ public class SmsUtil {
 		SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
 		sendSmsResponse.setPhoneCode(sms.getCode());
 		return sendSmsResponse;
-	}
-
-	public static QuerySendDetailsResponse querySendDetails(String bizId) throws ClientException {
-
-		// 可自助调整超时时间
-		System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
-		System.setProperty("sun.net.client.defaultReadTimeout", "10000");
-
-		// 初始化acsClient,暂不支持region化
-		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
-		DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
-		IAcsClient acsClient = new DefaultAcsClient(profile);
-
-		// 组装请求对象
-		QuerySendDetailsRequest request = new QuerySendDetailsRequest();
-		// 必填-号码
-		request.setPhoneNumber("15026513897");
-		// 可选-流水号
-		request.setBizId(bizId);
-		// 必填-发送日期 支持30天内记录查询，格式yyyyMMdd
-		SimpleDateFormat ft = new SimpleDateFormat("yyyyMMdd");
-		request.setSendDate(ft.format(new Date()));
-		// 必填-页大小
-		request.setPageSize(10L);
-		// 必填-当前页码从1开始计数
-		request.setCurrentPage(1L);
-
-		// hint 此处可能会抛出异常，注意catch
-		QuerySendDetailsResponse querySendDetailsResponse = acsClient.getAcsResponse(request);
-
-		return querySendDetailsResponse;
-	}
-
-	public static void main(String[] args) throws ClientException, InterruptedException {
-
-		// 发短信
-		SendSmsResponse response = sendSms("15026513897");
-		System.out.println("短信接口返回的数据----------------");
-		System.out.println("Code=" + response.getCode());
-		System.out.println("Message=" + response.getMessage());
-		System.out.println("RequestId=" + response.getRequestId());
-		System.out.println("BizId=" + response.getBizId());
-
-		Thread.sleep(3000L);
-
-		// 查明细
-		if (response.getCode() != null && response.getCode().equals("OK")) {
-			QuerySendDetailsResponse querySendDetailsResponse = querySendDetails(response.getBizId());
-			System.out.println("短信明细查询接口返回数据----------------");
-			System.out.println("Code=" + querySendDetailsResponse.getCode());
-			System.out.println("Message=" + querySendDetailsResponse.getMessage());
-			int i = 0;
-			for (QuerySendDetailsResponse.SmsSendDetailDTO smsSendDetailDTO : querySendDetailsResponse
-					.getSmsSendDetailDTOs()) {
-				System.out.println("SmsSendDetailDTO[" + i + "]:");
-				System.out.println("Content=" + smsSendDetailDTO.getContent());
-				System.out.println("ErrCode=" + smsSendDetailDTO.getErrCode());
-				System.out.println("OutId=" + smsSendDetailDTO.getOutId());
-				System.out.println("PhoneNum=" + smsSendDetailDTO.getPhoneNum());
-				System.out.println("ReceiveDate=" + smsSendDetailDTO.getReceiveDate());
-				System.out.println("SendDate=" + smsSendDetailDTO.getSendDate());
-				System.out.println("SendStatus=" + smsSendDetailDTO.getSendStatus());
-				System.out.println("Template=" + smsSendDetailDTO.getTemplateCode());
-			}
-			System.out.println("TotalCount=" + querySendDetailsResponse.getTotalCount());
-			System.out.println("RequestId=" + querySendDetailsResponse.getRequestId());
-		}
 	}
 
 	// 随机验证码
--- src/test/java/marykay/country/love/Test.java
+++ src/test/java/marykay/country/love/Test.java
@@ -3,13 +3,12 @@ package marykay.country.love;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
 import com.marykay.country.love.api.contract.request.QuerySendDetailsRequest;
-import com.marykay.country.love.api.contract.request.SendSmsRequest;
 import com.marykay.country.love.api.contract.response.QuerySendDetailsResponse;
 import com.marykay.country.love.api.contract.response.SendSmsResponse;
+import com.marykay.country.love.util.SmsUtil;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -31,47 +30,6 @@ public class Test {
 	// 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
 	static final String accessKeyId = "LTAIVWDsTO5sLf3H";
 	static final String accessKeySecret = "KNi1stkDkIoPl0p5C7dPJlkgz32o6N";
-
-	public static SendSmsResponse sendSms() throws ClientException {
-
-		// 可自助调整超时时间
-		System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
-		System.setProperty("sun.net.client.defaultReadTimeout", "10000");
-
-		// 初始化ascClient,暂时不支持多region（请勿修改）
-		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
-		DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
-		IAcsClient acsClient = new DefaultAcsClient(profile);
-
-		// 组装请求对象-具体描述见控制台-文档部分内容
-		SendSmsRequest request = new SendSmsRequest();
-		// 使用post提交
-		request.setMethod(MethodType.POST);
-		// 必填:待发送手机号
-		request.setPhoneNumbers("17702136954");
-		// 必填:短信签名-可在短信控制台中找到
-		request.setSignName("乡村爱情");
-		// 必填:短信模板-可在短信控制台中找到
-		request.setTemplateCode("SMS_131640001");
-		// 验证码
-		Test sms = new Test();
-		sms.setCode();
-		String codetemp = sms.getCode() + "";
-		String json = "{code:" + codetemp + "}";
-		// 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
-		request.setTemplateParam(json);
-
-		// 选填-上行短信扩展码(无特殊需求用户请忽略此字段)
-		// request.setSmsUpExtendCode("90997");
-
-		// 可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
-		request.setOutId("yourOutId");
-
-		// hint 此处可能会抛出异常，注意catch
-		SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
-
-		return sendSmsResponse;
-	}
 
 	public static QuerySendDetailsResponse querySendDetails(String bizId) throws ClientException {
 
@@ -107,7 +65,7 @@ public class Test {
 	public static void main(String[] args) throws ClientException, InterruptedException {
 
 		// 发短信
-		SendSmsResponse response = sendSms();
+		SendSmsResponse response = SmsUtil.sendSms("15026513897");
 		System.out.println("短信接口返回的数据----------------");
 		System.out.println("Code=" + response.getCode());
 		System.out.println("Message=" + response.getMessage());
